0e5f8cf4a430edafb14a7d575497a99e38882c10
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-03-01 01:26:14 +0530

    updat

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from '../AppRoutes';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BackgroundColorChanger } from "../Pages/BackgroundColorChanger";
+import Index from "../Pages/Index";
+import Calculator from "../Pages/Calculator";
 
-import '../styles/App.css';
+import "../styles/App.css";
 const App = () => {
-
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-
-  )
-}
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Index />,
+    },
+    {
+      path: "/bgcolor/:colorname",
+      element: <BackgroundColorChanger />,
+    },
+    {
+      path: "/calculator/",
+      element: <Calculator />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
 
 export default App;
